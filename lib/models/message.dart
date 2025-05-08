084dc051821ee47d5977d2a71370a2ec04e2bf48
john-xu-school <63273190+john-xu-school@users.noreply.github.com>
2025-05-08 12:58:32 -0700

    Implements basic chat functionality
    Adds Firebase authentication, Firestore integration,
    and basic chat features like sending, receiving,
    and deleting messages.

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Message {
+  final String id;
   final String userId;
   final String username;
   final String profileImageUrl;
@@ -9,6 +11,7 @@ class Message {
   final Message? replyTo; // Reference to the message being replied to
 
   Message({
+    required this.id,
     required this.userId,
     required this.username,
     required this.profileImageUrl,
@@ -16,4 +19,21 @@ class Message {
     required this.timestamp,
     this.replyTo,
   });
+
+  // Factory constructor to create a Message from Firestore data
+  factory Message.fromFirestore(Map<String, dynamic> data, String id) {
+    return Message(
+      id: id,
+      userId: data['userId'] ?? '',
+      username: data['username'] ?? 'Anonymous',
+      profileImageUrl: data['profileImageUrl'] ?? 'https://via.placeholder.com/50',
+      content: data['content'] ?? '',
+      timestamp: data['timestamp'] != null 
+          ? (data['timestamp'] as Timestamp).toDate()
+          : DateTime.now(),
+      replyTo: data['replyTo'] != null
+          ? Message.fromFirestore(data['replyTo'], data['replyTo']['id'] ?? '')
+          : null,
+    );
+  }
 } 
